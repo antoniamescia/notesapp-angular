68823a266fec70df089c4f39437b4917353125f5
jumpert <juan.perezc@correo.ucu.edu.uy>
2022-09-07 21:31:58 -0300

    Nuevo cambio

--- notesApp/src/app/app.module.ts
+++ notesApp/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AddButtonComponent } from './add-button/add-button.component';
 import { FooterComponent } from './footer/footer.component';
+import { ToggleDarkModeComponent } from './toggle-dark-mode/toggle-dark-mode.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AddButtonComponent,
-    FooterComponent
+    FooterComponent,
+    ToggleDarkModeComponent
   ],
   imports: [
     BrowserModule,
---
+++ notesApp/src/app/toggle-dark-mode/toggle-dark-mode.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dark-mode',
+  templateUrl: './toggle-dark-mode.component.html',
+  styleUrls: ['./toggle-dark-mode.component.css']
+})
+export class ToggleDarkModeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
